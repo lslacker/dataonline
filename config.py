4be2b1a8726f4ffb92cd73c18c2d24f46590ebb3
aslacker <aaron@melmailing.com.au>
2013-12-06 12:20:58 +1100

    Aaron tried to test

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ SECRET_KEY = os.urandom(24)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-#SQLALCHEMY_DATABASE_URI = 'postgresql://joboperators:abc123@localhost:5432/jobs'#
+SQLALCHEMY_DATABASE_URI = 'postgresql://joboperators:abc123@localhost:5432/jobs'
 
-#Testing-Aaron
+#Testing-Aaron1
